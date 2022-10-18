3db2420dac7221c03ac3d5e8e37fded8b001b25e
ujjwal1103 <ujjwallade11@gmail.com>
2022-10-18 14:35:06 +0530

    some change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ const Navbar = () => {
   return (
     <nav className="bg-amber-50 shadow-sm flex w-full ">
     <img src={logo} className="w-48 pt-3 pl-3 pb-3" alt="BigBasket"/>
+    <p>grocery</p>
     <div className='bg-slate-200 flex justify-start items-center p-4 w-96'>
         <input type="text" className='px-2 py-1 border-none w-full rounded' />
     </div>
